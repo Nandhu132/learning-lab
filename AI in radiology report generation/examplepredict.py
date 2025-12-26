cb1e494bdc6128444646c6728d301cccd70fada7
Nandhu132 <144427847+Nandhu132@users.noreply.github.com>
2025-12-26 14:24:45 +0530

    modify

--- AI in radiology report generation/examplepredict.py
+++ AI in radiology report generation/examplepredict.py
@@ -1,18 +1,16 @@
 import torch
-import torch.nn as nn
 from torchvision import models, transforms
 from PIL import Image
+import torch.nn as nn
 
-# ============ CONFIG ============
-MODEL_PATH = "densenet_normal_vs_pneumonia.pth"
-IMAGE_PATH = "C:/Users/Nandhini.M/Downloads/pneumonia.png"
-# ================================
+MODEL_PATH = "normal_vs_pneumonia.pth"
+IMAGE_PATH = "C:/Users/Nandhini.M/Downloads/pneumonia.png"   
+
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Transform (MUST MATCH TRAINING)
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
@@ -20,18 +18,15 @@ transform = transforms.Compose([
     )
 ])
 
-# Load model
 model = models.densenet121(weights=None)
 model.classifier = nn.Linear(model.classifier.in_features, 1)
 model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
 model = model.to(DEVICE)
 model.eval()
 
-# Load image
 img = Image.open(IMAGE_PATH).convert("RGB")
 img = transform(img).unsqueeze(0).to(DEVICE)
 
-# Predict
 with torch.no_grad():
     prob = torch.sigmoid(model(img)).item()
 
